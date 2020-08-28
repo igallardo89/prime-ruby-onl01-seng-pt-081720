41bc5bc970bd6ecb3c3fba1b53cf25b1b6d52bd9
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-28 14:22:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,16 @@
-def prime?(n):
-    if (n==1):
-        return False
-    elif (n==2):
-        return True;
-    else:
-        for x in range(2,n):
-            if(n % x==0):
-                return False
-        return True             
-  end
+
+def check_prime? (num)
+  prime=true
+ [2,3,5].each do |init|
+   if (num%init==0 && num!= init)
+   return false
+   end
+ end
+prime
 end
+
+def get_primes (num)
+  (1..num).select {|i| check_prime?(i)}
+end
+
+p get_primes(100)
