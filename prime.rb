220601c98796ffd303ed1bcb9e85e54a824439ea
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-28 14:21:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(n):
+    if (n==1):
+        return False
+    elif (n==2):
+        return True;
+    else:
+        for x in range(2,n):
+            if(n % x==0):
+                return False
+        return True             
+  end
+end
